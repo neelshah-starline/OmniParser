be278b222b1f798a630180c1d459b71fcaab3d53
Neel Shah <nshah@starlinewindows.com>
2025-11-07 08:25:54 -0800

    Remove paddleocr dependencies - comment out paddlepaddle/paddleocr in requirements and code

--- util/utils.py
+++ util/utils.py
@@ -18,17 +18,17 @@ import numpy as np
 # %matplotlib inline
 from matplotlib import pyplot as plt
 import easyocr
-from paddleocr import PaddleOCR
+# from paddleocr import PaddleOCR  # Commented out - using only easyocr
 reader = easyocr.Reader(['en'])
-paddle_ocr = PaddleOCR(
-    lang='en',  # other lang also available
-    use_angle_cls=False,
-    use_gpu=False,  # using cuda will conflict with pytorch in the same process
-    show_log=False,
-    max_batch_size=1024,
-    use_dilation=True,  # improves accuracy
-    det_db_score_mode='slow',  # improves accuracy
-    rec_batch_num=1024)
+# paddle_ocr = PaddleOCR(  # Commented out - using only easyocr
+#     lang='en',  # other lang also available
+#     use_angle_cls=False,
+#     use_gpu=False,  # using cuda will conflict with pytorch in the same process
+#     show_log=False,
+#     max_batch_size=1024,
+#     use_dilation=True,  # improves accuracy
+#     det_db_score_mode='slow',  # improves accuracy
+#     rec_batch_num=1024)
 import time
 import base64
 
